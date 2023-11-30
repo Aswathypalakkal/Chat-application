ca33b926bb380bc8864b7ca80ea4c92b8c513aad
Aswathypalakkal <aswathypalakkad69848@gmai.com>
2023-11-30 08:52:54 +0530

    Client design has been updated and chat functionality implemented

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { Socket } = require('dgram');
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
@@ -8,6 +9,18 @@ const io = socketIO(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 server.listen(3000, () => {
